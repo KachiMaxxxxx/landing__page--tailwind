cb5173576488576c2cd9087372ac01bf7af47a9b
KachiMaxxxxx <kachimax0101@gmail.com>
2025-06-01 23:12:53 +0100

    implemented functional nav bar with js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import "./input.css";
+
+
+const mobileNav = document.querySelector(".mnav");
+const closeBtn = document.querySelector(".mnav__close-btn");
+const closedBtnIcn = document.querySelector(".mnav__close-btn-icon");
+
+const navOpenedClass = "left-0";
+const navClosedClass = "-left-[300px]";
+const arrowLeftClass = "ri-arrow-right-s-line";
+const arrowRightClass = "ri-arrow-left-s-line";
+
+closeBtn.addEventListener("click", () =>{
+    if (mobileNav.classList.contains(navClosedClass)){
+        mobileNav.classList.toggle(navOpenedClass);
+
+        closedBtnIcn.classList.toggle(arrowLeftClass);
+        closedBtnIcn.classList.toggle(arrowRightClass);
+    }
+    });
