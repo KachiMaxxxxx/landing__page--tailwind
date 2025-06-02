b2a6d223e55cf360047ce1bb3137bda218ff48ff
KachiMaxxxxx <kachimax0101@gmail.com>
2025-06-02 02:47:03 +0100

    implemented header with nav an responsive components completed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import "./input.css";
 
-
 const mobileNav = document.querySelector(".mnav");
 const closeBtn = document.querySelector(".mnav__close-btn");
 const closedBtnIcn = document.querySelector(".mnav__close-btn-icon");
@@ -10,11 +9,11 @@ const navClosedClass = "-left-[300px]";
 const arrowLeftClass = "ri-arrow-right-s-line";
 const arrowRightClass = "ri-arrow-left-s-line";
 
-closeBtn.addEventListener("click", () =>{
-    if (mobileNav.classList.contains(navClosedClass)){
-        mobileNav.classList.toggle(navOpenedClass);
+closeBtn.addEventListener("click", () => {
+  if (mobileNav.classList.contains(navClosedClass)) {
+    mobileNav.classList.toggle(navOpenedClass);
 
-        closedBtnIcn.classList.toggle(arrowLeftClass);
-        closedBtnIcn.classList.toggle(arrowRightClass);
-    }
-    });
+    closedBtnIcn.classList.toggle(arrowLeftClass);
+    closedBtnIcn.classList.toggle(arrowRightClass);
+  }
+});
