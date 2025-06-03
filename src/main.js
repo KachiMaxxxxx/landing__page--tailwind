930089c4907a4a63c76400ec10c7bf74c8d37e05
KachiMaxxxxx <kachimax0101@gmail.com>
2025-06-03 22:45:28 +0100

    Started hero section

--- src/main.js
+++ src/main.js
@@ -1,19 +1,57 @@
 import "./input.css";
+document.addEventListener("DOMContentLoaded", () => {
+  const mobileNav = document.querySelector(".mnav");
+  const closeBtn = document.querySelector(".mnav__close-btn");
+  const closedBtnIcn = document.querySelector(".mnav__close-btn-icon");
+  
 
-const mobileNav = document.querySelector(".mnav");
-const closeBtn = document.querySelector(".mnav__close-btn");
-const closedBtnIcn = document.querySelector(".mnav__close-btn-icon");
+  const navOpenedClass = "left-0";
+  const navClosedClass = "-left-[300px]";
+  const arrowLeftClass = "ri-close-line";
+  const arrowRightClass = "ri-menu-2-line";
+  
+  closeBtn.addEventListener("click", () => {
+    const navIsClosed =  mobileNav.classList.contains(navClosedClass);
+    if (navIsClosed){
+        mobileNav.classList.remove(navClosedClass);
+        mobileNav.classList.add(navOpenedClass);
 
-const navOpenedClass = "left-0";
-const navClosedClass = "-left-[300px]";
-const arrowLeftClass = "ri-arrow-right-s-line";
-const arrowRightClass = "ri-arrow-left-s-line";
+        closedBtnIcn.classList.remove(arrowRightClass);
+        closedBtnIcn.classList.add(arrowLeftClass);
 
-closeBtn.addEventListener("click", () => {
-  if (mobileNav.classList.contains(navClosedClass)) {
-    mobileNav.classList.toggle(navOpenedClass);
 
-    closedBtnIcn.classList.toggle(arrowLeftClass);
-    closedBtnIcn.classList.toggle(arrowRightClass);
-  }
-});
+        
+      }
+      else{
+        mobileNav.classList.remove(navOpenedClass);
+        mobileNav.classList.add(navClosedClass);
+
+        closedBtnIcn.classList.remove(arrowLeftClass);
+        closedBtnIcn.classList.add(arrowRightClass);
+      }
+  });
+  document.addEventListener("click", (e) => {
+const navIsOpen = mobileNav.classList.contains(navOpenedClass);
+const clickedOutsideNav = !mobileNav.contains(e.target) && !closedBtnIcn.contains(e.target);
+
+if (clickedOutsideNav && navIsOpen){
+mobileNav.classList.remove(navOpenedClass);
+mobileNav.classList.add(navClosedClass);
+
+closedBtnIcn.classList.remove(arrowLeftClass);
+closedBtnIcn.classList.add(arrowRightClass);
+}
+
+
+
+
+
+  });
+  
+
+
+
+
+
+
+  });
